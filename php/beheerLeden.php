be56d1558f209a765abe2157d1c09731b2c19fae
LisaSpronk <47322637+LisaSpronk@users.noreply.github.com>
2020-11-18 16:18:00 +0100

    Update beheerLeden.php

--- php/beheerLeden.php
+++ php/beheerLeden.php
@@ -4,14 +4,5 @@ include("beheerNav.php");
 ?>
 <section>
 
-<?php while($post = $result->fetch_assoc()): ?>
-     <div class="post">
-
-        <img src="images/posts/<?php echo $post['image']; ?>">
-
-    </div>
-            
-
-<?php endwhile ?>
 
 </section>
