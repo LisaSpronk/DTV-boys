97e5a7b0cab63f9685b91dbe14e08851ac7b7f17
LisaDiks <47321976+LisaDiks@users.noreply.github.com>
2020-11-19 15:58:26 +0100

    header aanpassing
    Gelinkt aan goede pagina's en goede css bestanden.

--- Inloggen.php
+++ Inloggen.php
@@ -29,44 +29,8 @@
     <link rel="canonical" href="index.html">
     <meta property="og:url" content="index.html">
   </head>
-  <body class="u-body"><header class="u-align-right u-clearfix u-header u-header" id="sec-f56c"><div class="u-clearfix u-sheet u-sheet-1">
-        <a href="https://nicepage.com" class="u-image u-logo u-image-1">
-          <img src="images/default-logo.png" class="u-logo-image u-logo-image-1">
-        </a>
-        <nav class="u-align-right u-menu u-menu-dropdown u-offcanvas u-menu-1">
-          <div class="menu-collapse" style="font-size: 1.125rem; letter-spacing: 0px; font-weight: 700;">
-            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-custom-color-1 u-text-body-color u-text-hover-custom-color-1" href="#">
-              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
-              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
-</symbol>
-</defs></svg>
-            </a>
-          </div>
-          <div class="u-custom-menu u-nav-container">
-            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-custom-color-1 u-text-body-color u-text-hover-custom-color-1" href="Homepage.html" style="padding: 10px 20px;">Home</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-custom-color-1 u-text-body-color u-text-hover-custom-color-1" href="Banen-reserveren.html" style="padding: 10px 20px;">Banen reserveren</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-custom-color-1 u-text-body-color u-text-hover-custom-color-1" href="Toernooien.html" style="padding: 10px 20px;">Toernooien</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-custom-color-1 u-text-body-color u-text-hover-custom-color-1" href="Snackhoek.html" style="padding: 10px 20px;">Snackhoek</a>
-</li></ul>
-          </div>
-          <div class="u-custom-menu u-nav-container-collapse">
-            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
-              <div class="u-sidenav-overflow">
-                <div class="u-menu-close"></div>
-                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Homepage.html" style="padding: 10px 20px;">Home</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Banen-reserveren.html" style="padding: 10px 20px;">Banen reserveren</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Toernooien.html" style="padding: 10px 20px;">Toernooien</a>
-</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Snackhoek.html" style="padding: 10px 20px;">Snackhoek</a>
-</li></ul>
-              </div>
-            </div>
-            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
-          </div>
-        </nav>
-        <a href="Inloggen.html" data-page-id="22327776" class="u-btn u-btn-round u-button-style u-custom-color-1 u-radius-50 u-btn-1">Inloggen</a>
-        <a href="Registreren.html" data-page-id="236883568" class="u-btn u-btn-round u-button-style u-custom-color-3 u-radius-50 u-btn-2">Registreren</a>
-      </div>
-    </header>
+  <body class="u-body">
+    
 
     <div class="inloggen-pagina">
       <h1>Inloggen</h1>
