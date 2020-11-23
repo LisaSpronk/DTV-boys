a7748e560b13191a2f494097268ae60deb7b8a28
primordialboots <47321811+primordialboots@users.noreply.github.com>
2020-11-23 15:41:18 +0100

    Update Banen-reserveren.php

--- php/Banen-reserveren.php
+++ php/Banen-reserveren.php
@@ -1,10 +1,11 @@
 <?php
-  include('footer.php')
-  ?>
+  include("php/header.html");
 
-<section>
-</section>
+?>
 
-<?php
-  include('footer.php')
-  ?>
+    <div class="banen-reserveren">
+      
+    </div>
+    
+    
+    <footer class="u-align-center-md u-align-center-sm u-align-center-xs u-clearfix u-footer u-grey-80" id="sec-57e3"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
