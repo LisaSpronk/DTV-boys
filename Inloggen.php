64ce3adf1756c2bc3df64f8aeb3ee0bf67d9cceb
LisaDiks <47321976+LisaDiks@users.noreply.github.com>
2020-11-25 16:40:16 +0100

    inloggen personeel

--- Inloggen.php
+++ Inloggen.php
@@ -18,11 +18,11 @@
 
         <div class="login-personeel">
           <h3>Personeel</h3>
-            <form action="login.php" method="post" class="form-signin">
-                <input type="email" id="inputEmail" name="email" class="form-control" placeholder="E-mailadres" required autofocus>
-                <input type="password" id="inputWachtwoord" name="wachtwoord" class="form-control" placeholder="Wachtwoord" required>
-                <input  type="submit" class="btn-primary" value="Login">
-            </form>
+          <form action="login-personeel.php" method="post" class="form-signin">
+            <input type="email" id="inputEmail" name="personeel_email" class="form-control" placeholder="E-mailadres" required autofocus>
+            <input type="password" id="inputWachtwoord" name="personeel_wachtwoord" class="form-control" placeholder="Wachtwoord" required>
+            <input  type="submit" class="btn-primary" value="Login">
+          </form>
         </div>
       </div>
       <p class="p-registreren">Nog geen account?</p>
