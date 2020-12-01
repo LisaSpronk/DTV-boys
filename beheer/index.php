b87a85fcaa99d8c2833b194e05c307358f16bf4b
Teun Rensing <47321945+teunrensing@users.noreply.github.com>
2020-12-01 15:29:51 +0100

    Update

--- beheer/index.php
+++ beheer/index.php
@@ -1,5 +1,10 @@
-<?php include('beheer_header.php')?>
+<?php include('beheer_header.php');
+
+$stmt->prepare();
+
+
+?>
 <div id="content_beheer">
 
 </div>
-<?php include('beheer_footer.php')?>
+<?php include('beheer_footer.php');?>
